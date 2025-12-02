dabbe44f096e6086dda821e715288fdf2b8aeea0
Abhinand <abhinand.m@hatio.in>
2025-12-03 00:31:48 +0530

    Update base url

--- settleup-frontend/src/services/api.js
+++ settleup-frontend/src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // Create a central "client"
 const api = axios.create({
-    // baseURL: 'http://localhost:8080/api', //  Spring Boot URL local
+    baseURL: 'http://localhost:8080/api', //  Spring Boot URL local
 
-    baseURL: '/api',
+    // baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
